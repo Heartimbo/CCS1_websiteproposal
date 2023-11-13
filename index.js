b2a892cc223c77704e6f7933f763cfd866be5990
Heartimbo <Heartiimbo25@gmail.com>
2023-11-13 10:03:35 +0800

    Implement javascript on dropdown list.

---
+++ index.js
@@ -0,0 +1,9 @@
+var dropdown = document.getElementById("dropdown");
+
+function closedropdown() {
+    dropdown.style.top = "-100vh";
+}
+
+function opendropdown() {
+    dropdown.style.top = "17%";
+}
