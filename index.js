02ffc3fc32a759c385cf80ce4480f8ac0bc03e90
Heartimbo <Heartiimbo25@gmail.com>
2023-11-18 03:38:18 +0800

    Dropdown delay.

--- index.js
+++
@@ -1,9 +0,0 @@
-var dropdown = document.getElementById("dropdown");
-
-function closedropdown() {
-    dropdown.style.top = "-100vh";
-}
-
-function opendropdown() {
-    dropdown.style.top = "17%";
-}
